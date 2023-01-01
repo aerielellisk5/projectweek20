6c22d66265110010b2e0d3194471ef116545a0fb
Aeriel Ellis <aerielellisk5@gmail.com>
2022-12-31 19:15:52 -0800

    figuring out what needs to be done

---
+++ main.tf
@@ -0,0 +1,8 @@
+# things need to create an ec2 instance
+# pick a region
+# create a vpc
+# create some subnets
+# subnets need to be in 3 different availability zones
+# create security group
+# nat gateway or internet gateway
+# static webpage - probably ngninx 
